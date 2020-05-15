d9cc1bc5b4c4965d5a8fab75719d4276ea5b89d3
Valentina Panic <valentina.panic87@gmail.com>
2020-05-15 21:14:42 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,7 +2,7 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name, 
+        :name,
         :age
         #your key/value pairs here
         },
@@ -13,7 +13,7 @@ def third_challenge
         #your key/value pairs here
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
@@ -28,5 +28,5 @@ def third_challenge
    }
   }
 
-  
+
 end
